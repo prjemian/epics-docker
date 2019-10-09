98f6a37426a0a077aa68beaffe13175409d17ebc
Pete Jemian <prjemian@gmail.com>
2019-10-08 21:54:39 -0500

    configure for adsim

--- n4_areaDetector/start_adsim.sh
+++ n4_areaDetector/start_adsim.sh
@@ -19,65 +19,54 @@ CONTAINER=ioc${PRE}
 IMAGE=prjemian/synapps-6.1-ad-3.7
 
 # name of IOC manager (start, stop, status, ...)
-IOC_MANAGER=iocxxx/softioc/xxx.sh
+IOC_MANAGER=iocSimDetector/simDetector.sh
 
 # container will quit unless it has something to run
 # this is trivial but shows that container is running
 # prints date/time every 10 seconds
 KEEP_ALIVE_COMMAND="while true; do date; sleep 10; done"
 
-# pass the IOC PREFIX to the container ar boot time
+# pass the IOC PREFIX to the container at boot time
 ENVIRONMENT="PREFIX=${PREFIX}"
 
-# # convenience definitions
-# RUN="docker exec ${CONTAINER}"
-# TMP_ROOT=/tmp/docker_ioc
-# HOST_IOC_ROOT=${TMP_ROOT}/${CONTAINER}
-# IOC_TOP=${HOST_IOC_ROOT}/xxx-R6-1
-# OP_DIR=${TMP_ROOT}/synapps-6.1
-# # -------------------------------------------
+# convenience definitions
+RUN="docker exec ${CONTAINER}"
+TMP_ROOT=/tmp/docker_ioc
+HOST_IOC_ROOT=${TMP_ROOT}/${CONTAINER}
+IOC_TOP=${HOST_IOC_ROOT}/iocSimDetector
+OP_DIR=${TMP_ROOT}/synapps-6.1-ad-3.7
+# -------------------------------------------
 
-# # stop and remove container if it exists
-# remove_container.sh ${CONTAINER}
+# stop and remove container if it exists
+remove_container.sh ${CONTAINER}
 
-# echo -n "starting container ${CONTAINER} ... "
-# docker run -d --net=host \
-#     --name ${CONTAINER} \
-#     -e "${ENVIRONMENT}" \
-#     ${IMAGE} \
-#     bash -c "${KEEP_ALIVE_COMMAND}"
+echo -n "starting container ${CONTAINER} ... "
+docker run -d --net=host \
+    --name ${CONTAINER} \
+    -e "${ENVIRONMENT}" \
+    ${IMAGE} \
+    bash -c "${KEEP_ALIVE_COMMAND}"
 
-# sleep 1
+sleep 1
 
-# # edit files in docker IOC for use with GUI software
-# echo "changing xxx: to ${PREFIX} in ${CONTAINER}"
-# CMD="${RUN} sed -i s:/APSshare/bin/caQtDM:caQtDM:g iocxxx/../../start_caQtDM_xxx"; ${CMD}
-# CMD="${RUN} sed -i s/xxx:/${PREFIX}/g iocxxx/../../xxxApp/op/adl/xxx.adl"; ${CMD}
-# CMD="${RUN} sed -i s/ioc=xxx/ioc=${PRE}/g iocxxx/../../xxxApp/op/adl/xxx.adl"; ${CMD}
-# CMD="${RUN} sed -i s/xxx:/${PREFIX}/g iocxxx/../../xxxApp/op/ui/xxx.ui"; ${CMD}
-# CMD="${RUN} sed -i s/ioc=xxx/ioc=${PRE}/g iocxxx/../../xxxApp/op/ui/xxx.ui"; ${CMD}
+echo -n "starting IOC ${CONTAINER} ... "
+CMD="${RUN} ${IOC_MANAGER} start"
+echo ${CMD}
+${CMD}
+sleep 2
 
-# echo -n "starting IOC ${CONTAINER} ... "
-# CMD="${RUN} ${IOC_MANAGER} start"
-# echo ${CMD}
-# ${CMD}
-# sleep 2
+# copy container's files to /tmp for medm & caQtDM
+echo "copy IOC ${CONTAINER} to ${HOST_IOC_ROOT}"
+mkdir -p ${HOST_IOC_ROOT}
+docker cp ${CONTAINER}:/opt/synApps/support/areaDetector-R3-7/ADSimDetector/iocs/simDetectorIOC/iocBoot/iocSimDetector/  ${HOST_IOC_ROOT}
+mkdir -p ${OP_DIR}
+docker cp ${CONTAINER}:/opt/synApps/support/screens/   ${OP_DIR}
 
-# # copy container's files to /tmp for medm & caQtDM
-# echo "copy IOC ${CONTAINER} to ${HOST_IOC_ROOT}"
-# mkdir -p ${HOST_IOC_ROOT}
-# docker cp ${CONTAINER}:/opt/synApps/support/xxx-R6-1/  ${HOST_IOC_ROOT}
-# mkdir -p ${OP_DIR}
-# docker cp ${CONTAINER}:/opt/synApps/support/screens/   ${OP_DIR}
+# edit files in docker container IOC for use with GUI software
+echo "changing 13SIM1: to ${PREFIX} in ${CONTAINER}"
+sed -i s+_IOC_SCREEN_DIR_+`echo ${IOC_TOP}`+g ${IOC_TOP}/start_caQtDM_adsim
+sed -i s+_AD_SCREENS_DIR_+`echo ${OP_DIR}/screens/ui`+g ${IOC_TOP}/start_caQtDM_adsim
+sed -i s+"# CAQTDM_DISPLAY_PATH"+CAQTDM_DISPLAY_PATH+g ${IOC_TOP}/start_caQtDM_adsim
 
-# # best to define these find/replace steps in pieces
-# # adjust the starter for caQtDM
-# FIND="source \${EPICS_APP}/setup_epics_common caqtdm"
-# # interpret the macros in the next string
-# REPLACE=`echo "export CAQTDM_DISPLAY_PATH=${IOC_TOP}/xxxApp/op/ui:${OP_DIR}/screens/ui"`
-# sed -i s+"${FIND}"+"${REPLACE}"+g ${IOC_TOP}/start_caQtDM_xxx
-
-# # adjust the starter for MEDM
-# FIND="source \${EPICS_APP}/setup_epics_common medm"
-# REPLACE=`echo "export EPICS_DISPLAY_PATH=${IOC_TOP}/xxxApp/op/adl:${OP_DIR}/screens/adl"`
-# sed -i s+"${FIND}"+"${REPLACE}"+g ${IOC_TOP}/start_MEDM_xxx
+# TODO: find the caQtDM plugins and copy locally
+# TODO: edit QT_PLUGIN_PATH in start_caQtDM_adsim
