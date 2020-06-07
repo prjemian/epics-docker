a98549b93157d7ef8464752b9e5617663688d712
Pete R. Jemian <prjemian@gmail.com>
2020-06-07 09:40:51 -0500

    WIP #6

--- n3_synApps/ioc_scripts/custom_ioc_xxx.sh
+++ n3_synApps/ioc_scripts/custom_ioc_xxx.sh
@@ -2,3 +2,10 @@
 
 # build a custom IOC: xxx
 echo "# TODO: build a custom IOC: xxx"
+
+# COPY ./ioc_scripts/iocAdminSoft_aliases.db .
+# COPY ./ioc_scripts/iocAdminSoft_aliases.iocsh .
+
+# TODO: add components from start_xxx.sh here
+# fix for https://github.com/epics-modules/xxx/issues/30                                     ${XXX}/iocBoot/iocxxx/common.iocsh
+# RUN sed -i s+"IOC=\$(PREFIX)\")"+"IOC=\$(PREFIX)\")\n< iocAdminSoft_aliases.iocsh"+g ${XXX}/iocBoot/iocxxx/st.cmd.Linux
