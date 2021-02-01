623e8ec059ad7e913c6f788ea3915791091a51fc
Pete R. Jemian <prjemian@gmail.com>
2021-02-01 16:38:21 -0600

    MNT #16 try with a motor bugfix branch

--- v1.1/n3_synApps/ioc_files/edit_assemble_synApps.sh
+++ v1.1/n3_synApps/ioc_files/edit_assemble_synApps.sh
@@ -62,7 +62,7 @@ _MODULES_+=" XXX"
 #   eval ${cmd}
 # done
 for mod in "MOTOR"; do
-  cmd="sed -i s:'^${mod}=\S*\$':'${mod}=master':g assemble_synApps.sh"
+  cmd="sed -i s:'^${mod}=\S*\$':'${mod}=${MOTOR_HASH}':g assemble_synApps.sh"
   echo ${cmd}
   eval ${cmd}
 done
